1d547d7a8e9135cd798b081c4dca017733d7e378
vargasdm <112888008+vargasdm@users.noreply.github.com>
2023-01-10 17:58:51 -0500

    removed node_modules from repo

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,7 @@
 
 const mysql = require('mysql2');
-// const Sequelize = require('sequelize');
 require('dotenv').config();
 
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: 'localhost',
-//     dialect: 'mysql',
-//     port: 3306
-//   }
 const db = mysql.createConnection(
     {
         host: process.env.DB_HOST,
